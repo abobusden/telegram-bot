7809da535acc3af937663fd0d8746bfb275debf3
abobusden <abobusden3@gmail.com>
2026-09-18 21:18:13 +0300

    Update states.py

--- states.py
+++ states.py
@@ -5,3 +5,20 @@ class Reg(StatesGroup):
     nickname = State()
     gender = State()
     faction = State()
+
+
+class Bank(StatesGroup):
+    topup = State()
+    withdraw = State()
+    transfer_account = State()
+    transfer_amount = State()
+    deposit = State()
+
+
+class Casino(StatesGroup):
+    dice_bet = State()
+    slots_bet = State()
+
+
+class Race(StatesGroup):
+    bet = State()
